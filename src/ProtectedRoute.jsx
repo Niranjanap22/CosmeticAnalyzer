05fb6741f7e784f737b116f0604a8be435df3c55
Nipuna C Deepu <214774222+NIPUNADEEPU@users.noreply.github.com>
2026-02-17 11:59:43 +0530

    Delete src directory

--- src/ProtectedRoute.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-
-export default function ProtectedRoute({ user, children }) {
-  // show loading only while auth status is unknown (undefined)
-  if (user === undefined) return <div>Loading...</div>;
-  return user ? children : <Navigate to="/login" replace />;
-}
